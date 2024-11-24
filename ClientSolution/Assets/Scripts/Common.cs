b7fcdbc1c18fd3179e77d696e87ad15b8ef1e06b
mup <brodover@gmail.com>
2024-11-24 18:55:50 +0900

    Added Mainmenu scene

---
+++ ClientSolution/Assets/Scripts/Common.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public static class Common
+    {
+        public const string ServerUrl = "https://localhost:7083/api/Matchmaking";
+
+        public enum Role
+        {
+            None,
+            Human,
+            Cat,
+            Random
+        }
+    }
+}
--- ServerSolution/Server/Program.cs
+++ ServerSolution/Server/Program.cs
@@ -1,35 +1,54 @@
+using Server.Handlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSignalR();
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+
+// Configure logging
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+//builder.Logging.AddDebug();
+//if (builder.Environment.IsDevelopment())
+//    builder.Logging.SetMinimumLevel(LogLevel.Debug);
+//else
+//    builder.Logging.SetMinimumLevel(LogLevel.Error);
+
 
 var app = builder.Build();
 
-app.UseWebSockets();
-
 app.UseSwagger();
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerUI();
-
-    app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
+}
+else
+{
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+/*app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.UseCors("CorsPolicy");
+
+app.UseRouting();*/
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
